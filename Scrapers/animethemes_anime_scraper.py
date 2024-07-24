ac51a2b9f69dd87f670c529b1d32244567dc9e0d
cslchee <cheetahspeed939@gmail.com>
2024-07-23 22:36:32 -0700

    Updated animethemes_anime_scraper in order to update oped_anime_data.json. Basically just changed the class #'s required to find info during scraping. Removed the scraper and data for Series from github since it's not useful.

--- Scrapers/animethemes_anime_scraper.py
+++ Scrapers/animethemes_anime_scraper.py
@@ -1,6 +1,5 @@
 """
  Same logic as 'animethemes_series_scrap', but it EVERYTHING. Skips 'Seasons' page
- TODO Currently not catching a song's author (i.e. SONG NAME  by  BAND). It's all separate HTML elements
 """
 import json, requests
 from random import uniform
@@ -33,7 +32,7 @@ def scrape_all_anime():
     soup = BeautifulSoup(requests.get(base_url+'/anime').text, "html.parser")
     wait_a_sec()
 
-    all_anime = soup.findAll('a', {'class': 'sc-d86917e8-0 bVdVPS'}) #This might change!
+    all_anime = soup.findAll('a', {'class': 'sc-330991ae-0 EawiL'}) #This might change!
     if len(all_anime) == 0:
         input("Couldn't find any anime on the main page, the class names have probably changed again!  >_")
         exit()
@@ -64,7 +63,7 @@ def scrape_all_anime():
 
         #Get and process the synopsis
         try:
-            synopsis = soup.find('p', {'class': 'sc-76ad0c61-0 dQNwzr'}).text
+            synopsis = soup.find('p', {'class': 'sc-330991ae-0 dILlfA'}).text
             try:
                 synopsis = synopsis[:synopsis.index('(Source')]
             except ValueError as e:
@@ -75,15 +74,16 @@ def scrape_all_anime():
         data[anime_name]['synopsis'] = synopsis
 
         # Get date/titles/studios.   Alt Titles -> Premiere -> Series (Optional) -> Studios -> Links
-        info = [x.strip() for x in soup.find('dl', {'class': 'sc-93902f8-0 cOHMIw'}).prettify().split('\n') if '>' not in x]
+        info = [x.strip() for x in soup.find('dl', {'class': 'sc-268ce5b-0 fIChyp'}).prettify().split('\n') if '>' not in x]
         #using x.text puts all the alt titles into one string with on transition
         data[anime_name]['alt titles'] = info[1:info.index('Premiere')]
         data[anime_name]['date'] = info[info.index('Premiere')+1]
         data[anime_name]['studios'] = info[info.index('Studios')+1:info.index('Links')] if 'Studios' in info else 'NA'
         data[anime_name]['series'] = info[info.index('Series')+1] if 'Series' in info else 'NA'
 
+
         #Get AniList Genres - It the classes change: Box of links class, link element class
-        sidebar_links = soup.findAll('div', {'class': 'sc-b08d39b6-0 cmkFNw'})[-1].findAll('a', {'class':'sc-d86917e8-0 cLKGBD'})
+        sidebar_links = soup.findAll('div', {'class': 'sc-cc7f7bed-0 jJjoUS'})[-1].findAll('a', {'class':'sc-330991ae-0 frvDJG'})
         anilist_link = ""
         for sb_l in sidebar_links:
             if 'anilist' in sb_l['href']:
@@ -100,21 +100,21 @@ def scrape_all_anime():
         else:
             print("\t\tCould not find an AniList link for this anime.")
 
-        vid_divs = soup.findAll('div', {'class': 'sc-47964127-0 sc-f91d738-0 sc-e475d769-0 epJOQu ldUmdf zxBOP'})
+        vid_divs = soup.findAll('div', {'class': 'sc-32d9eff6-0 sc-b0ae8233-0 sc-60d82f02-0 gIUKKm hYpoBS ivgSKj'})
         for vid in vid_divs:
-            op_ed = 'op' if ('OP' in vid.find('small', {'class': 'sc-d86917e8-0 gMupXm'}).text) else 'ed'
+            op_ed = 'op' if ('OP' in vid.find('small', {'class': 'sc-330991ae-0 bpEVpP'}).text) else 'ed'  #look at the tiny <small> box
             try:
-                vid_name = vid.find('span',{'class': 'sc-d86917e8-0 hKmOyJ'}).text
+                vid_name = vid.find('span',{'class': 'sc-330991ae-0 eErKmH'}).text
                 #Add song artist's if there is any
-                if len(vid.findAll('small', {'class': 'sc-d86917e8-0 aGhNy'})) != 0: #look for 'by'
-                    vid_name += f' by {vid.findAll("a", {"class": "sc-d86917e8-0 hKmOyJ"})[0].text}' #Note the 'a' when looking for the class
+                if len(vid.findAll('small', {'class': 'sc-330991ae-0 fnypQK'})) != 0: #look for 'by'
+                    vid_name += f' by {vid.findAll("a", {"class": "sc-330991ae-0 frvDJG"})[0].text}' #Note the 'a' when looking for the class
             except AttributeError as e:
                 vid_name = 'NA'
-            vid_sources = [x['href'] for x in vid.findAll('a', {'class': 'sc-aa5e92bc-0 sc-aa5e92bc-2 eJSgxI gWHZqy'})]
+            vid_sources = [x['href'] for x in vid.findAll('a', {'class': 'sc-b86cc7cc-0 sc-b86cc7cc-3 gwoTAk bewfwf'})]
             vid_splits = [x for x in vid.prettify().split('\n') if '<svg' in x or '<' not in x]
 
-            #Get 'data-icon="film"' indexes. The text for 'sc-76ad0c61...' episodes label will appear 1 index later
-            labels = [vid_splits[count+1].strip() for count, x in enumerate(vid_splits) if 'data-icon="film"' in x]
+            #Get op/ed episodes label indexes. The numbers next to 'film' icon. The text for the episodes  will appear 1 index later than the svg does
+            labels = [vid_splits[count+1].strip() for count, x in enumerate(vid_splits) if 'data-fa="fas-film" role="img"' in x]
 
             # OP/ED Source   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
             for v_src, label in zip(vid_sources, labels): #First split has 'OP1' label
@@ -122,7 +122,7 @@ def scrape_all_anime():
                     label = '---' # \u2014
                 soup = BeautifulSoup(requests.get(base_url+v_src).text, "html.parser")
                 wait_a_sec()
-                file_source = soup.find('video', {'class': 'sc-9cfb0b81-1 ekthvd'})['src']
+                file_source = soup.find('video', {'class': 'sc-1b838190-4 bOqWZv'})['src']
                 file_source = file_source[file_source.index('.moe/')+5:-5] #remove 'https://v.animethemes.moe/' and '.webm'
                 print(f'\t\t{op_ed.upper()}: {vid_name} - {label}  -->  {file_source}')
                 data[anime_name][op_ed][vid_name] = {label: file_source}
